7adace75ae3e2189c467b8331c9a86ff69470069
D1monik <116081418+D1monik@users.noreply.github.com>
2024-04-26 11:18:11 +0200

    3.feladat

--- fuvar.py
+++ fuvar.py
@@ -11,9 +11,13 @@ class fuvar:
     def __str__(self):
         return f"{self.azon},{self.indul},{self.ido},{self.tav},{self.dij},{self.borra},{self.mod}"
 
-
-
-
-
-
 f = open("fuvar.csv", "rt", encoding="UTf-8")
+f.readline
+
+lista = []
+
+for sor in f:
+    sor = sor.strip().split(";")
+    lista.append(fuvar(sor[0],sor[1],sor[2],sor[3],sor[4],sor[5],sor[6],))
+
+print(f"3.feladat: {len(lista)- 1} fuvar")
